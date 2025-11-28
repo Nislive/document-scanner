eb7a3b9df6d043f34ab6e8330b51f5206110ffcb
Alperen <enisalprn@gmail.com>
2025-11-28 14:13:39 +0300

    detect largest document and order corner points

--- main.py
+++ main.py
@@ -9,6 +9,25 @@ edges = cv2.Canny(median_blurred, 140, 200)
 
 kernel= np.ones((3,3), np.uint8)
 img_dilation = cv2.dilate(edges, kernel, iterations=1)
-cv2.imshow("Display window", img_dilation)
+
+contours, hierarchy = cv2.findContours(img_dilation, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+contours = sorted(contours, key=cv2.contourArea, reverse=True)
+
+target_contour = None
+
+for contour in contours:
+    perimeter = cv2.arcLength(contour, True)
+    epsilon = 0.02*perimeter
+    approx = cv2.approxPolyDP(contour, epsilon, True)
+
+    if len(approx) == 4:
+        target_contour=approx
+        break
+
+pts = target_contour.reshape(4,2)
+rect = np.zeros((4,2), dtype="float32")
+
+cv2.imshow("Display window", pts)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
